4e90e3e0e776455cbbf56b59224af591922cf7b4
Your Name <romansk2001@gmail.com>
2024-03-12 18:25:54 +0200

    Implemented winget search

--- main.js
+++ main.js
@@ -1,31 +1,78 @@
-const open = require('./node_modules/open')
+const { exec } = require("child_process");
 
-const { method, parameters } = JSON.parse(process.argv[2])
+const { method, parameters } = JSON.parse(process.argv[2]);
+
+function formatWingetSearchResults(stdout) {
+  const lines = stdout.split("\n");
+  const packages = [];
+  const headerString = "Name" + lines[0].split("Name")[1];
+  NamePlacement = [
+    headerString.indexOf("Name"),
+    headerString.indexOf("Id") - 1,
+  ];
+  IdPlacement = [
+    headerString.indexOf("Id"),
+    headerString.indexOf("Version") - 1,
+  ];
+  Source = [headerString.indexOf("Source"), headerString.length - 1];
+
+  for (let i = 2; i < lines.length - 2; i++) {
+    const name = lines[i].slice(NamePlacement[0], NamePlacement[1]).trim();
+    const id = lines[i].slice(IdPlacement[0], IdPlacement[1]).trim();
+    const source = lines[i].slice(Source[0], Source[1]).trim();
+    packages.push({ name, id, source });
+  }
+  return packages;
+}
 
 if (method === "query") {
-	console.log(JSON.stringify(
-		{
-			"result": [{
-				"Title": "Hello World Typescript",
-				"Subtitle": "Showing your query parameters: " + parameters + ". Click to open Flow's website",
-				"JsonRPCAction": {
-                    "method": "do_something_for_query",
-                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
-                },
-				"IcoPath": "Images\\app.png"
-			}]
-		}
-	));
+  const userInput = parameters;
+
+  if (userInput.toString() === "") {
+    console.log(
+      JSON.stringify({
+        result: [
+          {
+            Title: "Enter a package name to search",
+            Subtitle: "Click on package to install it.",
+            JsonRPCAction: {
+              method: "query",
+              parameters: [""],
+            },
+            IcoPath: "Images\\app.png",
+          },
+        ],
+      })
+    );
+    return;
+  } else {
+    exec(`winget search --name ${userInput} -n 10`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing winget search: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`winget search stderr: ${stderr}`);
+        return;
+      }
+
+      const packages = formatWingetSearchResults(stdout);
+
+      const results = packages.map((package) => ({
+        Title: package.name,
+        Subtitle: `ID: ${package.id} | Source: ${package.source}`,
+        JsonRPCAction: {
+          method: "install_package",
+          parameters: [package],
+        },
+        IcoPath: "Images\\app.png",
+      }));
+      1;
+
+      console.log(JSON.stringify({ result: results }));
+    });
+  }
 }
 
-if (method === "do_something_for_query") {
-	url = parameters[0]
-	do_something_for_query(url)
+if (method === "install_package") {
 }
-
-function do_something_for_query(url) {
-	open(url);
-}
-
- 
-
